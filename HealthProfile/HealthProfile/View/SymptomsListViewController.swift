95721c8512db00846d8dafc687312cbd2b815917
Gimin Moon <gmoon@usc.edu>
2018-02-17 17:40:29 -0800

    Added gitignore

---
+++ HealthProfile/HealthProfile/View/SymptomsListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SymptomsListViewController.swift
+//  HealthProfile
+//
+//  Created by Gimin Moon on 2/17/18.
+//  Copyright © 2018 Gimin Moon. All rights reserved.
+//
+
+import UIKit
+
+class SymptomsListViewController: UIViewController, UITableViewDelegate{
+
+    var symptoms : [String]?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //symptoms = APIMedicController.getSymptoms()
+    }
+    
+    
+
+   
+
+}
